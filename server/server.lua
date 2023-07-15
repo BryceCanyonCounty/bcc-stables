299f2acf0260f36a98de304576b7f5e979bb6f7e
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-14 21:50:31 -0700

    update server.lua
    - Add new job check
    - update buy and save function for escaping horse purchase
    - Housekeeping

--- server/server.lua
+++ server/server.lua
@@ -3,20 +3,7 @@ local VORPInv = {}
 TriggerEvent('getCore', function(core)
     VORPcore = core
 end)
-
 VORPInv = exports.vorp_inventory:vorp_inventoryApi()
-
-RegisterNetEvent('bcc-stables:GetMyHorses', function()
-    local src = source
-    local Character = VORPcore.getUser(src).getUsedCharacter
-    local identifier = Character.identifier
-    local charid = Character.charIdentifier
-
-    MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
-        function(horses)
-            TriggerClientEvent('bcc-stables:ReceiveHorsesData', src, horses)
-        end)
-end)
 
 RegisterNetEvent('bcc-stables:BuyHorse', function(data)
     local src = source
@@ -26,35 +13,30 @@ RegisterNetEvent('bcc-stables:BuyHorse', function(data)
     local maxHorses = Config.maxHorses
 
     MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
-        function(horses)
-            if #horses >= maxHorses then
-                VORPcore.NotifyRightTip(src, _U('horseLimit') .. maxHorses .. _U('horses'), 5000)
+    function(horses)
+        if #horses >= maxHorses then
+            VORPcore.NotifyRightTip(src, _U('horseLimit') .. maxHorses .. _U('horses'), 5000)
+            TriggerClientEvent('bcc-stables:StableMenu', src)
+            return
+        end
+        if data.IsCash then
+            if Character.money >= data.Cash then
+                TriggerClientEvent('bcc-stables:SetHorseName', src, data, false)
+            else
+                VORPcore.NotifyRightTip(src, _U('shortCash'), 5000)
                 TriggerClientEvent('bcc-stables:StableMenu', src)
                 return
             end
-            Wait(200)
-
-            if data.IsCash then
-                local cashPrice = data.Cash
-                if Character.money >= cashPrice then
-                    Character.removeCurrency(0, cashPrice)
-                else
-                    VORPcore.NotifyRightTip(src, _U('shortCash'), 5000)
-                    TriggerClientEvent('bcc-stables:StableMenu', src)
-                    return
-                end
+        else
+            if Character.gold >= data.Gold then
+                TriggerClientEvent('bcc-stables:SetHorseName', src, data, false)
             else
-                local goldPrice = data.Gold
-                if Character.gold >= goldPrice then
-                    Character.removeCurrency(1, goldPrice)
-                else
-                    VORPcore.NotifyRightTip(src, _U('shortGold'), 5000)
-                    TriggerClientEvent('bcc-stables:StableMenu', src)
-                    return
-                end
+                VORPcore.NotifyRightTip(src, _U('shortGold'), 5000)
+                TriggerClientEvent('bcc-stables:StableMenu', src)
+                return
             end
-            TriggerClientEvent('bcc-stables:SetHorseName', src, data, false)
-        end)
+        end
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:BuyTack', function(data)
@@ -89,17 +71,23 @@ RegisterNetEvent('bcc-stables:SaveNewHorse', function(data, name)
     local charid = Character.charIdentifier
 
     MySQL.Async.execute('INSERT INTO player_horses (identifier, charid, name, model, gender) VALUES (?, ?, ?, ?, ?)',
-        { identifier, charid, tostring(name), data.ModelH, data.gender },
-        function(done)
-        end)
+    { identifier, charid, tostring(name), data.ModelH, data.gender },
+    function(done)
+        if data.IsCash then
+            Character.removeCurrency(0, data.Cash)
+        else
+            Character.removeCurrency(1, data.Gold)
+        end
+        TriggerClientEvent('bcc-stables:StableMenu', src)
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:UpdateHorseName', function(data, name)
-    local horseId = data.horseId
-
-    MySQL.Async.execute('UPDATE player_horses SET name = ? WHERE id = ?', { name, horseId },
-        function(done)
-        end)
+    local src = source
+    MySQL.Async.execute('UPDATE player_horses SET name = ? WHERE id = ?', { name, data.horseId },
+    function(done)
+        TriggerClientEvent('bcc-stables:StableMenu', src)
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:SelectHorse', function(id)
@@ -109,23 +97,19 @@ RegisterNetEvent('bcc-stables:SelectHorse', function(id)
     local charid = Character.charIdentifier
 
     MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
-        function(horse)
-            for i = 1, #horse do
-                local horseId = horse[i].id
-                MySQL.Async.execute(
-                    'UPDATE player_horses SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?',
-                    { 0, identifier, charid, horseId },
+    function(horse)
+        for i = 1, #horse do
+            local horseId = horse[i].id
+            MySQL.Async.execute('UPDATE player_horses SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 0, identifier, charid, horseId },
+            function(done)
+                if horse[i].id == id then
+                    MySQL.Async.execute('UPDATE player_horses SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 1, identifier, charid, id },
                     function(done)
-                        if horse[i].id == id then
-                            MySQL.Async.execute(
-                                'UPDATE player_horses SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?',
-                                { 1, identifier, charid, id },
-                                function(done)
-                                end)
-                        end
                     end)
-            end
-        end)
+                end
+            end)
+        end
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:GetSelectedHorse', function()
@@ -135,18 +119,29 @@ RegisterNetEvent('bcc-stables:GetSelectedHorse', function()
     local charid = Character.charIdentifier
 
     MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
-        function(horses)
-            if #horses ~= 0 then
-                for i = 1, #horses do
-                    if horses[i].selected == 1 then
-                        TriggerClientEvent('bcc-stables:SetHorseInfo', src, horses[i].model, horses[i].name,
-                            horses[i].components, horses[i].id, horses[i].gender)
-                    end
+    function(horses)
+        if #horses ~= 0 then
+            for i = 1, #horses do
+                if horses[i].selected == 1 then
+                    TriggerClientEvent('bcc-stables:SetHorseInfo', src, horses[i].model, horses[i].name, horses[i].components, horses[i].id, horses[i].gender)
                 end
-            else
-                VORPcore.NotifyRightTip(src, _U('noHorses'), 5000)
             end
-        end)
+        else
+            VORPcore.NotifyRightTip(src, _U('noHorses'), 5000)
+        end
+    end)
+end)
+
+RegisterNetEvent('bcc-stables:GetMyHorses', function()
+    local src = source
+    local Character = VORPcore.getUser(src).getUsedCharacter
+    local identifier = Character.identifier
+    local charid = Character.charIdentifier
+
+    MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
+    function(horses)
+        TriggerClientEvent('bcc-stables:ReceiveHorsesData', src, horses)
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:UpdateComponents', function(components, horseId, MyHorse_entity)
@@ -157,10 +152,10 @@ RegisterNetEvent('bcc-stables:UpdateComponents', function(components, horseId, M
     local encodedComponents = json.encode(components)
 
     MySQL.Async.execute('UPDATE player_horses SET components = ? WHERE identifier = ? AND charid = ? AND id = ?',
-        { encodedComponents, identifier, charid, horseId },
-        function(done)
-            TriggerClientEvent('bcc-stables:SetComponents', src, MyHorse_entity, components)
-        end)
+    { encodedComponents, identifier, charid, horseId },
+    function(done)
+        TriggerClientEvent('bcc-stables:SetComponents', src, MyHorse_entity, components)
+    end)
 end)
 
 RegisterNetEvent('bcc-stables:SellHorse', function(id)
@@ -171,26 +166,26 @@ RegisterNetEvent('bcc-stables:SellHorse', function(id)
     local modelHorse = nil
 
     MySQL.Async.fetchAll('SELECT * FROM player_horses WHERE identifier = ? AND charid = ?', { identifier, charid },
-        function(horses)
-            for i = 1, #horses do
-                if tonumber(horses[i].id) == tonumber(id) then
-                    modelHorse = horses[i].model
-                    MySQL.Async.execute('DELETE FROM player_horses WHERE identifier = ? AND charid = ? AND id = ?',
-                        { identifier, charid, id },
-                        function(done)
-                            for _, horseConfig in pairs(Config.Horses) do
-                                for models, values in pairs(horseConfig.colors) do
-                                    if models == modelHorse then
-                                        local sellPrice = (Config.sellPrice * values.cashPrice)
-                                        Character.addCurrency(0, sellPrice)
-                                        VORPcore.NotifyRightTip(src, _U('soldHorse') .. sellPrice, 5000)
-                                    end
-                                end
+    function(horses)
+        for i = 1, #horses do
+            if tonumber(horses[i].id) == tonumber(id) then
+                modelHorse = horses[i].model
+                MySQL.Async.execute('DELETE FROM player_horses WHERE identifier = ? AND charid = ? AND id = ?', { identifier, charid, id },
+                function(done)
+                    for _, horseConfig in pairs(Config.Horses) do
+                        for models, values in pairs(horseConfig.colors) do
+                            if models == modelHorse then
+                                local sellPrice = (Config.sellPrice * values.cashPrice)
+                                Character.addCurrency(0, sellPrice)
+                                VORPcore.NotifyRightTip(src, _U('soldHorse') .. sellPrice, 5000)
                             end
-                        end)
-                end
+                        end
+                    end
+                end)
             end
-        end)
+        end
+        TriggerClientEvent('bcc-stables:StableMenu', src)
+    end)
 end)
 
 -- Inventory
@@ -228,14 +223,31 @@ VORPInv.RegisterUsableItem('oil_lantern', function(data)
     TriggerClientEvent('bcc-stables:UseLantern', src, data)
 end)
 
--- Job Check
-RegisterNetEvent('bcc-stables:GetPlayerJob', function()
+-- Check if Player has Required Job
+VORPcore.addRpcCallback('CheckPlayerJob', function(source, cb, shop)
     local src = source
     local Character = VORPcore.getUser(src).getUsedCharacter
-    local CharacterJob = Character.job
-    local CharacterGrade = Character.jobGrade
+    local playerJob = Character.job
+    local jobGrade = Character.jobGrade
 
-    TriggerClientEvent('bcc-stables:SendPlayerJob', src, CharacterJob, CharacterGrade)
+    if playerJob then
+        for _, job in pairs(Config.shops[shop].allowedJobs) do
+            if playerJob == job then
+                if tonumber(jobGrade) >= tonumber(Config.shops[shop].jobGrade) then
+                    cb(true)
+                else
+                    VORPcore.NotifyRightTip(src, _U('needJobGrade'), 4000)
+                    cb(false)
+                end
+            else
+                VORPcore.NotifyRightTip(src, _U('needJob'), 4000)
+                cb(false)
+            end
+        end
+    else
+        VORPcore.NotifyRightTip(src, _U('needJob'), 4000)
+        cb(false)
+    end
 end)
 
 --- Check if properly downloaded
@@ -245,7 +257,6 @@ function file_exists(name)
 end
 
 if not file_exists('./ui/index.html') then
-    print("^1 INCORRECT DOWNLOAD!  ^0")
-    print(
-        '^4 Please Download: ^2(bcc-stables.zip) ^4from ^3<https://github.com/BryceCanyonCounty/bcc-stables/releases/latest>^0')
+    print('^1 INCORRECT DOWNLOAD!  ^0')
+    print('^4 Please Download: ^2(bcc-stables.zip) ^4from ^3<https://github.com/BryceCanyonCounty/bcc-stables/releases/latest>^0')
 end
